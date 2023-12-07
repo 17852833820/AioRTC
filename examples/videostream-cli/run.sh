055d77b3775f6139db4576783239c63c5a0d4a28
ying <zuoyingying_sdu@163.com>
2023-12-07 16:16:36 +0800

    change connection mode to socket

--- examples/videostream-cli/run.sh
+++ examples/videostream-cli/run.sh
@@ -1,8 +1,8 @@
 发送端
 cd aiortc
-python3 ./examples/videostream-cli/cli.py offer --play-from ./dataset/test.mp4 -v
+python3 ./examples/videostream-cli/cli.py offer --play-from ./dataset/test.mp4 -v -s tcp-socket --signaling-port 2222
 
 接收端
 cd aiortc
-python3 ./examples/videostream-cli/cli.py answer --record-to ./receive_data/video.mp4 -v
+sleep 3 && python3 ./examples/videostream-cli/cli.py answer --record-to ./receive_data/video.mp4 -v -s tcp-socket --signaling-port 2222
 
